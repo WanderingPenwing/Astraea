996d021a7dbb98078092665f0cec8af71ebea34e
WanderingPenwing <nicolas.pinson31@gmail.com>
2024-10-05 09:37:47 +0200

    black screen

--- src/main.rs
+++ src/main.rs
@@ -1,57 +1,45 @@
+//! A simple 3D scene with light shining over a cube sitting on a plane.
+
 use bevy::prelude::*;
+use bevy::render::*;
+use bevy::math::*;
 
 fn main() {
     App::new()
-    	.add_plugins(DefaultPlugins)
-    	.add_systems(Startup, setup)
-    	.add_systems(Update, move_player)
-    	.run();
+        .add_plugins(DefaultPlugins)
+        .add_systems(Startup, setup)
+        .run();
 }
 
-#[derive(Bundle)]
-struct PlayerBundle {
-	core: Player,
-    sprite: SpriteBundle,
-}
-
-#[derive(Component)]
-struct Player;
-
-
+/// set up a simple 3D scene
 fn setup(
-	//mut materials: ResMut<Assets<ColorMaterial>>,
-	//mut meshes: ResMut<Assets<Mesh>>,
-	mut commands: Commands,
-	asset_server: Res<AssetServer>
+    mut commands: Commands,
+    mut meshes: ResMut<Assets<Mesh>>,
+    mut materials: ResMut<Assets<StandardMaterial>>,
 ) {
-	commands.spawn(Camera2dBundle::default());
-	commands.spawn(PlayerBundle {
-		core: Player,
-		sprite: SpriteBundle {
-			texture: asset_server.load("../assets/test.png"),
-			..default()
-		},
-	});
+
+	let star_material = materials.add(Color::WHITE);
+
+	let star = meshes.add(Plane3d::new(Vec3::Y, Vec2::splat(1.0)));
+	
+    // circular base
+    commands.spawn(MaterialMeshBundle {
+        mesh: star,
+        material: star_material.clone(),
+        transform: Transform::from_xyz(0.0, 1.0, 0.0),
+        ..default()
+    });
+    // light
+    commands.spawn((
+        PointLight {
+            shadows_enabled: true,
+            ..default()
+        },
+        Transform::from_xyz(0.0, 0.8, 1.0),
+    ));
+    // camera
+    commands.spawn((
+        Camera3d::default(),
+        Transform::from_xyz(0.0, 0.0, 0.0).looking_at(Vec3::ZERO, Vec3::Y),
+    ));
 }
-
-const MOVE_SPEED: f32 = 6.0;
-
-fn move_player(
-	mut transforms: Query<&mut Transform, With<Player>>,
-	keys: Res<ButtonInput<KeyCode>>,
-) {
-	for mut transform in transforms.iter_mut() {
-		let mut direction = Vec3::ZERO;
-
-		if keys.pressed(KeyCode::KeyW) { direction.y += 1.0; }
-		if keys.pressed(KeyCode::KeyA) { direction.x -= 1.0; }
-		if keys.pressed(KeyCode::KeyS) { direction.y -= 1.0; }
-		if keys.pressed(KeyCode::KeyD) { direction.x += 1.0; }
-		
-		if 0.0 < direction.length() {
-			transform.translation += MOVE_SPEED * direction.normalize();
-		}
-	}
-}
-
-
